e39d8389c361b5ca85a6e9a542e1acc3d34a7d88
samroodAli <devsamrood@gmail.com>
2021-06-10 02:45:05 +0530

    Fix:Fix link to reddit

--- reddit.mjs
+++ reddit.mjs
@@ -5,9 +5,23 @@ import yargs from 'yargs'
 
 const { argv } = yargs(process.argv)
 
+console.log(argv)
+
 const res = await fetch('https://reddit.com/.json')
 const data = await res.json()
 
 const children = data.data.children
 const randomIndex = Math.floor(Math.random() * data.data.children.length)
 const post = children[randomIndex]
+const link = `https://reddit.com${post.data.permalink}`
+
+if (argv.print) {
+  console.log(
+    `
+    title: ${post.data.title}
+    link: ${link}
+    `
+  ) 
+} else {
+  open(link)
+}
